e235472a31ff6b24378dc84da37156c56b6cd29a
shayani-web <shayanineth4@gmail.com>
2025-05-27 13:30:34 +0930

    Adding more arguments to the loop to compare results

--- run_all.py
+++ run_all.py
@@ -9,15 +9,25 @@ experiments = [
 # Define which algorithms to run
 runs = ["fedasync", "fedleo"]
 
+# Define the number of local epochs
+L_epochs =["10", "20", "30"]
+
+# Define Batch Size
+Batch_S=["8", "20", "32"]
+
 # Loop through all combinations
 for exp in experiments:
     for run in runs:
-        print(f"\nRunning {run} with model={exp['model']}, dataset={exp['dataset']}, IID={exp['iid']}")
-        subprocess.run([
-            "python", "src/federated_main.py",
-            "--model", exp["model"],
-            "--dataset", exp["dataset"],
-            "--iid", exp["iid"],
-            "--epochs", "1",
-            "--run", run
-        ])
+        for local_ep in L_epochs:
+            for bs in Batch_S:
+                print(f"\nRunning {run} with model={exp['model']}, dataset={exp['dataset']}, IID={exp['iid']}")
+                subprocess.run([
+                    "python", "src/federated_main.py",
+                    "--model", exp["model"],
+                    "--dataset", exp["dataset"],
+                    "--iid", exp["iid"],    #IID/NON-IID data
+                    "--epochs", "1",    #Define the number of global rounds
+                    "--local_ep", local_ep,     # Define the number of local epochs
+                    "--local_bs", bs,       # Batch size
+                    "--run", run
+                ])
